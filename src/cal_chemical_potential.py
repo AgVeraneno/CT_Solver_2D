a12dbc7e614546290bdd4c99140ea15cf0bb2b81
AgVeraneno <30921alex@gmail.com>
2020-02-19 23:57:15 +0800

    tuning find mu function

--- src/cal_chemical_potential.py
+++ src/cal_chemical_potential.py
@@ -17,26 +17,36 @@ class CP_solver():
         '''
         calculate carrier concentration @ 0K
         '''
+        t0 = time.time()
         with Pool(processes=int(setup['CPU_threads'])) as mp:
             ND = mp.map(self.cal_concentration, range(mesh_size))
         nD = sum(ND)/(4*np.pi**2)
-        print('Carrier concentration:',nD,' 1/cm2')
+        t1 = time.time()
+        print('Carrier concentration:',nD*1e4,' 1/cm2; calculated time=',round(t1-t0,4),' (sec)')
         '''
         find chemical potential
         '''
-        self.mu = self.Ef
+        self.mu = 3*self.Ef
+        pre_mu = 0
+        up_mu = self.mu
+        down_mu = pre_mu
         counter = 0
-        while True:
+        while True and counter <= 100:
             with Pool(processes=int(setup['CPU_threads'])) as mp:
                 NCV = mp.map(self.find_chemical_potential, range(mesh_size))
-            NC = sum(np.array(NCV)[:,0])
-            NV = sum(np.array(NCV)[:,1])
-            print('Try mu=',self.mu/self.mat.q*1e3,' (meV) --> difference = ',round(np.real(nD-NC+NV),6))
-            if float(nD-NC+NV) <= 1 or counter >= 100:
+            NC = sum(np.array(NCV)[:,0])/(4*np.pi**2)
+            NV = sum(np.array(NCV)[:,1])/(4*np.pi**2)
+            print('Try mu=',round(self.mu/self.mat.q*1e3,3),' (meV) --> difference = ',round(np.real(nD-NC+NV),6))
+            pre_mu = copy.deepcopy(self.mu)
+            if abs(nD-NC+NV) <= 1:
                 break
-            else:
-                self.mu -= 0.01*self.mat.q*1e-3
-                counter += 1
+            elif np.real(nD-NC+NV) < 0:
+                self.mu = (up_mu+down_mu)/2
+                up_mu = self.mu
+            elif np.real(nD-NC+NV) > 0:
+                self.mu = (up_mu+down_mu)/2
+                down_mu = self.mu
+            counter += 1
     def cal_concentration(self, k_idx):
         H_parser = lib_material.Hamiltonian(self.setup)
         e_count = 0
