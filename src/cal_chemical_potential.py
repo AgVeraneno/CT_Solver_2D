67f18602d9c289e3c4bd4989a22c63ccc0293e12
AgVeraneno <30921alex@gmail.com>
2020-02-22 17:47:41 +0800

    improve converge algorithm

--- src/cal_chemical_potential.py
+++ src/cal_chemical_potential.py
@@ -26,10 +26,8 @@ class CP_solver():
         '''
         find chemical potential
         '''
-        self.mu = 500*1.6e-19
-        pre_mu = 0
-        up_mu = self.mu
-        down_mu = pre_mu
+        self.mu = 2*self.Ef
+        down_mu = 0
         pre_dn = 0
         counter = 0
         while counter <= 100:
@@ -38,15 +36,13 @@ class CP_solver():
             NC = np.real(sum(np.array(NCV)[:,0])/(4*np.pi**2))
             NV = np.real(sum(np.array(NCV)[:,1])/(4*np.pi**2))
             print('Try mu=',round(self.mu/self.mat.q*1e3,9),' (meV) --> difference = ',round(nD-NC+NV,6),'(NC=',NC,';NV=',NV,')')
-            pre_mu = copy.deepcopy(self.mu)
-            if abs(nD-NC+NV) <= 1 or round(abs(nD-NC+NV)-pre_dn,6) == 0:
+            if abs(nD-NC+NV) <= 1:# or round(abs(nD-NC+NV)-pre_dn,6) == 0:
                 break
-            elif np.real(nD-NC+NV) < 0:
-                self.mu = (up_mu+down_mu)/2
-                up_mu = self.mu
-            elif np.real(nD-NC+NV) > 0:
-                self.mu = (up_mu+down_mu)/2
-                down_mu = self.mu
+            elif np.real(nD-NC+NV) < 0:     # valance < conduction. reduce mu
+                self.mu = (self.mu+down_mu)/2
+            elif np.real(nD-NC+NV) > 0:     # valance > conduction. increase mu
+                down_mu = copy.deepcopy(self.mu)
+                self.mu += 0.001*1.6e-19
             pre_dn = abs(nD-NC+NV)
             counter += 1
     def cal_concentration(self, k_idx):
@@ -58,13 +54,13 @@ class CP_solver():
             E_Kp = np.sort(E[0:4])[2]   # get first conduction band energy
             E_Kn = np.sort(E[4:8])[2]   # get first conduction band energy
             if E_Kp <= self.Ef:
-                e_count += 3
+                e_count += 1
             else:
-                e_count += 2
+                e_count += 0
             if E_Kn <= self.Ef:
-                e_count += 3
+                e_count += 1
             else:
-                e_count += 2
+                e_count += 0
         else:
             return e_count
     def find_chemical_potential(self, k_idx):
@@ -74,11 +70,26 @@ class CP_solver():
         for ky in self.ky_mesh:
             H = H_parser.FZ_bulk(self.gap, self.V, self.kx_mesh[k_idx], ky)
             E, _ = np.linalg.eig(H)
-            for i in range(4):
-                nc_count += 1/(1+np.exp((np.sort(E[0:4])[i]-self.mu)/(self.Temp)))
-                nv_count += (1-1/(1+np.exp((np.sort(E[0:4])[i]-self.mu)/(self.Temp))))
-                nc_count += 1/(1+np.exp((np.sort(E[4:8])[i]-self.mu)/(self.Temp)))
-                nv_count += (1-1/(1+np.exp((np.sort(E[4:8])[i]-self.mu)/(self.Temp))))
+            if self.Temp/self.mat.kB <= 1:
+                for i in range(4):
+                    if np.sort(E[0:4])[i] <= self.mu:
+                        nc_count += 1
+                        nv_count += 0
+                    else:
+                        nc_count += 0
+                        nv_count += 1
+                    if np.sort(E[4:8])[i] <= self.mu:
+                        nc_count += 1
+                        nv_count += 0
+                    else:
+                        nc_count += 0
+                        nv_count += 1
+            else:
+                for i in range(4):
+                    nc_count += 1/(1+np.exp((np.sort(E[0:4])[i]-self.mu)/(self.Temp)))
+                    nv_count += (1-1/(1+np.exp((np.sort(E[0:4])[i]-self.mu)/(self.Temp))))
+                    nc_count += 1/(1+np.exp((np.sort(E[4:8])[i]-self.mu)/(self.Temp)))
+                    nv_count += (1-1/(1+np.exp((np.sort(E[4:8])[i]-self.mu)/(self.Temp))))
         else:
             return nc_count, nv_count
 
